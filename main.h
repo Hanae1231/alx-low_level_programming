df4a7ad58b97fdbd3e06ab77160750c4074db95c
Hanae <ennajihana1@gmail.com>
2023-10-02 14:05:40 -0700

    mmM

--- main.h
+++ main.h
@@ -1,11 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+int _putchar(char c);
+int _atoi(char *s);
 
-void positive_or_negative(int i);
-int largest_number(int, int, int);
-void print_remaining_days(int, int, int);
-int convert_day(int, int);
+#endif
 
-#endif /* MAIN_H */
